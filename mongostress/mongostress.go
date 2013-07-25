70fe0752f78a00ae2430c2d75c670ab2e1899b87
Peter Buchmann <peter.buchmann@idealo.de>
2013-07-25 18:00:31 +0200

    webfrontend extended 2

--- mongostress/mongostress.go
+++ mongostress/mongostress.go
@@ -1341,3 +1341,13 @@ func renderTemplate( w http.ResponseWriter,tname string , c map[string]string ){
 	Flush1(w)
 	
 }
+
+func Reverse(s string) string{
+	b := []byte(s)
+	for i:= 0; i < len(b)/2; i++ {
+		j := len(b)-i-1
+		b[i],b[j] = b[j],b[i]
+	}
+	return string(b)
+
+}
