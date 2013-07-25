f85167d7de19d1348ef3e5c77c30f2282c541311
Peter <peter.buchmann@web.de>
2013-07-25 17:58:18 +0200

    Delete local_single_instance.sh

--- hello/local_single_instance.sh
+++
@@ -1,6 +0,0 @@
-#not ssh idealo@sx122 -A - call from local
-
-#cd ~/mongo/program/bin; ./mongod        --shell --host sx122:27017
-cd ~/mongo/program/bin; ./mongod       --host localhost
-cd ~/mongo/program/bin; ./mongo        --shell --host localhost
-
